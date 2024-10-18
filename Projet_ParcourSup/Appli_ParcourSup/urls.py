77a47f1399aab4e961e9e031807f5db4f783239d
Zeyroxxx34 <147750064+Zeyroxxx34@users.noreply.github.com>
2024-10-18 09:34:38 +0200

    Supprimer une formation

--- Projet_ParcourSup/Appli_ParcourSup/urls.py
+++ Projet_ParcourSup/Appli_ParcourSup/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('index/', views.Bonjour_view, name='Page Accueil'),  
-    path('formations/', views.formations_view, name='liste_formations'),  # Ce nom doit correspondre à la vue formations_view
+    path('formations/', views.formations_view, name='liste_formations'),
     path('index/formations/page/ajouter/', views.ajouter_formation, name='ajouter_formation'),  
     path('formations/modifier/<int:pk>/', views.modifier_formation, name='modifier_formation'),  
     path('formations/supprimer/<int:pk>/', views.supprimer_formation, name='supprimer_formation'),  
